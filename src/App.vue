0850e07a60c6379f567b046fdbb04332ea088bba
Wilfried N'SOUKPOE <wilfriedproject@gmail.com>
2022-07-06 14:28:35 +0000

    Mise en place du frontend vue

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+
+
+<template>
+ <div>
+   <span>
+     hello world 
+     je suis le compteur et pour le moment je suis à {{count}}
+   </span>
+
+   <button @click="increment()">Incrémenter</button>
+   <router-view></router-view>
+ </div>
+
+  
+</template>
+<script>
+export default ({
+  data() {
+    return {
+      count: 0
+    }
+    
+  },
+
+  methods: {
+    increment: function(){
+     this.count++
+    }
+  }
+})
+</script>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+
+</style>
