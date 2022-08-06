3da70d76e2cbb2de59416c9b33b98e822b15b3fa
root <root@localhost.localdomain>
2022-08-06 00:13:21 +0000

    Feature: add comment

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,16 @@
 import { createStore } from 'vuex'
 import Cookies from 'js-cookie'
-import axios  from 'axios'
-
+import axios from 'axios'
 const connected = Cookies.get('isConnected')
 const user_info = Cookies.get('user')
 const store = createStore({
-    state(){
-        const post_slug = 'premier-post'
+     state(){
         const api = "http://127.0.0.1:3333"
 
         return {
             isConnected: connected == undefined ? false :  true, 
             user: user_info !=  undefined ? Cookies.get('user') : null, 
-            posts:  axios.get(`${api}/${post_slug}`).then(response => response)
+            post:   axios(`${api}/posts`).then(response => {return response.data})
 
         }
     }, 
