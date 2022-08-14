a28a4e7a1b3dcd1167bef5c8cbd592e47506e948
root <root@localhost.localdomain>
2022-08-14 00:12:10 +0000

    Create post validator

--- app/Controllers/Http/BlogController.ts
+++ app/Controllers/Http/BlogController.ts
@@ -3,6 +3,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Comment from 'App/Models/Comment';
 import Post from 'App/Models/Post';
 import Drive from '@ioc:Adonis/Core/Drive'
+import CreatePostValidator from 'App/Validators/CreatePostValidator';
 
 export default class BlogController {
 
@@ -10,24 +11,31 @@ export default class BlogController {
     client = 'http://127.0.0.1:3000';
     server = 'http://127.0.0.1:3333'
     public async createPost({ request, response }: HttpContextContract) {
-        const thumbnail = request.file('file')
-        const post = {
-            name : request.input('name').trim(), 
-            description: request.input('description').trim(),
-            slug: request.input('name').trim().split(' ').join('-').toLowercase(),
-            content: request.input('content'), 
-            thumbnail: `${this.server}/downloads/${thumbnail?.clientName}`, 
-            userId: request.cookie('user').id
-        }
-          
-        await Post.create(post)
+        try {
+            const thumbnail = request.file('file')
+            const post = {
+                name : request.input('name').trim(), 
+                description: request.input('description').trim(),
+                slug: request.input('name').trim().split(' ').join('-').toLowercase(),
+                content: request.input('content'), 
+                thumbnail: `${this.server}/downloads/${thumbnail?.clientName}`, 
+                userId: request.cookie('user').id
+            }
+            await request.validate(CreatePostValidator)
+            await Post.create(post)
 
-        if (thumbnail != null) {
-            await thumbnail.move(Application.tmpPath('uploads'), {
-                name: post.thumbnail
-            })
-            
+            if (thumbnail != null) {
+                await thumbnail.move(Application.tmpPath('uploads'), {
+                    name: post.thumbnail
+                })
+                
+            }
+        } catch (error) {
+            return {
+                message: error.message
+            }
         }
+        
       
         response.redirect().toPath(this.client)
 
