d665889e6d8a4c81273990c6cc6fb584a1223120
Wilfried N'SOUKPOE <wilfriednsoukpoe1@gmail.com>
2022-09-02 16:56:30 +0000

    Design user comments

--- src/pages/PostDetail.vue
+++ src/pages/PostDetail.vue
@@ -1,19 +1,19 @@
 <template>
-  <div  v-for="post in posts" :key="post.id" class="post w-full h-auto bg-gray- flex flex-col ">
-    
+  <div v-for="post in posts" :key="post.id" class="post w-full h-auto bg-gray- flex flex-col ">
+
 
     <!-- post -->
     <div style='height:400px;' class='w-full bg-blue- flex flex-col justify-center items-center bg-green-'>
       <!-- post name -->
       <div class="post__title flex justify-center items-center bg-red-">
-        <div  class="bg-violet- justify-center"><span class="font-bold text-6xl">{{post.name}}</span></div>
+        <div class="bg-violet- justify-center"><span class="font-bold text-6xl">{{ post.name }}</span></div>
       </div>
 
       <!-- post description -->
       <div class="post__description bg-yellow- flex justify-center">
         <div class="bg-gray-">
           <span class="text-center">400
-            {{post.description}}
+            {{ post.description }}
           </span>
         </div>
       </div>
@@ -21,7 +21,7 @@
       <!-- post infos -->
       <div class="post__infos">
         <div class="flex justify-around bg-green-">
-             <PostBadge :post='post' />
+          <PostBadge :post='post' />
         </div>
       </div>
     </div>
@@ -29,135 +29,162 @@
     <!--description -->
     <div class="post__content bg-green- flex justify-center">
       <div class="rounded ">
-        {{post.content}}
+        {{ post.content }}
       </div>
     </div>
 
     <div class="post__comments bg-yellow- w-full flex justify-center items-center">
       <div class="rounded">
         <div class="w-full  h-12 flex items-center pl-4">
-          <span>{{post.comments.length}} Commentaires</span>
+          <span>{{ post.comments.length }} Commentaires</span>
         </div>
         <hr>
-         <div >
-           <div v-for="comment in post.comments" :key="comment.id" style='height:100px;' class="flex mt-2 w-full bg-green-">
-             <div class="w-24 bg-orange- flex justify-center items- h-full ">
-               <div style="width:60px; height:60px;" class="bg-gray-300 flex overflow-hidden justify-center items-center rounded-full">
-                 <font-awesome-icon class='text-white mt-3 text-5xl' icon="fa-solid fa-user" />
-               </div>
-             </div>
 
-             <div class="w-full bg-violet- h-full ">
-               <div class="post__comment__name w-full h-auto  bg-yellow- flex  items-end">
-                 <span class="text-md capitalize font-medium">
-                  
-                   {{comment.users.username}} 
-                 </span>
-                 
-                 <span class="text-gray-400 ml-2">{{moment(comment.created_at).fromNow()}}</span>
-               </div>
+        <div style="padding: 30px;">
 
-               <div class="post__comment__description">
-                 <span class="text-gray-400">
-                   {{comment.message}}
-                 </span>
-               </div>
-             </div>
+          <ol class="relative border-l border-gray-200 dark:border-gray-700">
 
-            
-           </div>
-          </div>
+            <li v-for="comment in post.comments" :key="comment.id" class="mb-10 ml-6">
+              <span class="flex absolute -left-3 justify-center items-center w-6 h-6 rounded-full">
+                <div style="width:30px; height:30px;" class=" flex overflow-hidden justify-center items-center ">
+                  <font-awesome-icon class='text-gray-300 mt-3 text-5xl' icon="fa-solid fa-user" />
+                </div>
+              </span>
 
-          <form action="http://127.0.0.1:3333/comments" method="post" v-if="isConnected">
-              <div>
-                <label for="message">MEssage:</label>
-                <input type="text" name="post_id" :value="post.id" class="hidden" />
-                <textarea
-                  name="message"
-                  draggable="false"
-                  cols="30"
-                  rows="10"
-                ></textarea>
+              <div
+                class="p-4 bg-white rounded-lg border border-gray-200 shadow-sm dark:bg-gray-700 dark:border-gray-600">
+                <div class="justify-between items-center mb-3 sm:flex">
+                  <time class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">{{
+                      moment(comment.created_at).fromNow()
+                  }}</time>
+                  <div class="text-sm font-normal text-gray-500 lex dark:text-gray-300">{{ comment.users.username }}
+                    commented </div>
+                </div>
+                <div
+                  class="p-3 text-xs italic font-normal text-gray-500 bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-600 dark:border-gray-500 dark:text-gray-300">
+                  {{ comment.message }}</div>
               </div>
-              <button
-                type="submit"
-                class="bg-blue-400 px-3 py-2 rounded text-white"
-              >
-                Envoyer
-              </button>
-            </form>
+            </li>
 
-          <div class="w-full h-28 flex justify-center items-center" v-if='!isConnected'>
-            <router-link to="/user/login">
-              <button  class="text-white bg-blue-400 font-bold rounded uppercase  py-3 px-3 ml-2 ">Connectez vous pour envoyer un commentaire</button>
-            </router-link>
+          </ol>
+
+
+        </div>
+
+        <div v-if="isConnected" style="padding:30px">
+          <div>
+            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Votre
+              message</label>
+
+            <input type="text" name="post_id" :value="post.id" class="hidden" />
+
+            <textarea name="message" rows="4"
+            style="resize: none; "
+            v-model="message"
+              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:border-blue-500"
+              placeholder="Laisser un commentaire"></textarea>
+
           </div>
+          <button v-on:click="sendComment(post.id)" type="submit" class="bg-blue-400 px-3 mt-3 py-2 rounded text-white">
+            Envoyer
+          </button>
+        </div>
+
+        <div class="w-full h-28 flex justify-center items-center" v-if='!isConnected'>
+          <router-link to="/user/login">
+            <button  class="text-white bg-blue-400 font-bold rounded uppercase  py-3 px-3 ml-2 ">Connectez vous pour
+              envoyer un commentaire</button>
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <style lang="scss">
-@import '../assets/styles/_variables.scss'; 
-@import '../assets/styles/_mixins.scss'; 
+@import '../assets/styles/_variables.scss';
+@import '../assets/styles/_mixins.scss';
+
 .post {
   background: $primary-bg;
   font-family: $primary-font;
 
 }
-.post__title,.post__description, .post__infos{
+
+.post__title,
+.post__description,
+.post__infos {
   width: $full-width;
   @include center-horizontally;
-  height: auto; 
-  padding: 10px; 
-  > div {
-  @include center-horizontally; 
+  height: auto;
+  padding: 10px;
+
+  >div {
+    @include center-horizontally;
   }
 }
+
 .post__title {
-  align-items:end;
+  align-items: end;
   color: $primary-color;
-  > div {
-      width: 1000px;
+
+  >div {
+    width: 1000px;
   }
 }
+
 .post__description {
-  > div {
+  >div {
     width: 800px;
   }
 }
-.post__infos> div {
+
+.post__infos>div {
   width: 500px;
 }
-.post__content > div {
+
+.post__content>div {
   width: 1000px;
   height: auto;
   padding-bottom: 100px;
 }
+
 .post__comments {
   height: auto;
-  padding: 10px ;
-  > div {
+  padding: 10px;
+
+  >div {
     width: 1000px;
     height: auto;
     background: white;
   }
 }
-  
 </style>
 
 <script>
 import PostBadge from '@/components/PostBadge.vue'
 import moment from 'moment'
 export default {
-  components: {PostBadge}, 
+  components: { PostBadge },
   data() {
     return {
       posts: [],
       isConnected: this.$store.state.isConnected,
-      moment: moment
+      moment: moment, 
+      message: ''
     };
   },
-  methods: {},
+  methods: {
+    sendComment(hello){
+      // alert("voilà le post id", postId);
+      this.axios.post("http://127.0.0.1:3333/comments", {
+        message: this.message, 
+        postId: hello,
+        userId: this.$cookies.get("user")
+      }).then(response => {
+        console.log(response)
+      })
+    }
+  },
   created() {
     this.$store.state.posts.then((result) => {
       return result
@@ -168,6 +195,6 @@ export default {
     });
   },
 
-  mounted() {},
+  mounted() { },
 };
 </script>
