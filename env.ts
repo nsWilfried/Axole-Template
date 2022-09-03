545146db36d6b8a0d809f8d3f2a5863c8e634e0b
Wilfried N'SOUKPOE <wilfriednsoukpoe1@gmail.com>
2022-09-03 17:37:26 +0000

    Final upload of my rest api

--- env.ts
+++ env.ts
@@ -16,7 +16,7 @@ import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
 	// HOST: Env.schema.string({ format: 'host' }),
-	// PORT: Env.schema.number(),
+	PORT: Env.schema.number(),
 	APP_KEY: Env.schema.string(),
 	APP_NAME: Env.schema.string(),
   	DRIVE_DISK: Env.schema.enum(['local'] as const),
