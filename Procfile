48d93f99346d550d66bc4baa8f8c46697ddb7b23
Wilfried N'SOUKPOE <wilfriednsoukpoe1@gmail.com>
2022-09-03 16:02:44 +0000

    Try to fix bug

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: node ace migration:run --force
-web: npm start
+release: ENV_SILENT=true node ace migration:run --force
+web:  npm start
--- env.ts
+++ env.ts
@@ -15,22 +15,22 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
-	HOST: Env.schema.string({ format: 'host' }),
-	PORT: Env.schema.number(),
+	// HOST: Env.schema.string({ format: 'host' }),
+	// PORT: Env.schema.number(),
 	APP_KEY: Env.schema.string(),
 	APP_NAME: Env.schema.string(),
   	DRIVE_DISK: Env.schema.enum(['local'] as const),
     SESSION_DRIVER: Env.schema.string(),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-	SMTP_HOST: Env.schema.string({ format: 'host' }),
-	SMTP_PORT: Env.schema.string(),
-	SMTP_USERNAME: Env.schema.string(),
-	SMTP_PASSWORD: Env.schema.string(),
-PG_HOST: Env.schema.string({ format: 'host' }),
-PG_PORT: Env.schema.number(),
-PG_USER: Env.schema.string(),
-PG_PASSWORD: Env.schema.string.optional(),
-PG_DB_NAME: Env.schema.string(),
+	// SMTP_HOST: Env.schema.string({ format: 'host' }),
+	// SMTP_PORT: Env.schema.string(),
+	// SMTP_USERNAME: Env.schema.string(),
+	// SMTP_PASSWORD: Env.schema.string(),
+// PG_HOST: Env.schema.string({ format: 'host' }),
+// PG_PORT: Env.schema.number(),
+// PG_USER: Env.schema.string(),
+// PG_PASSWORD: Env.schema.string.optional(),
+// PG_DB_NAME: Env.schema.string(),
 
 })
 
