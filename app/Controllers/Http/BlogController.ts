454a8b751111b50d171c7ffdd70fd98ad2fc151e
Wilfried N'SOUKPOE <wilfriednsoukpoe1@gmail.com>
2022-09-05 09:45:51 +0000

    Fix build fail

--- app/Controllers/Http/BlogController.ts
+++ app/Controllers/Http/BlogController.ts
@@ -50,7 +50,7 @@ export default class BlogController {
         // console.log("tout s'est bien passé", data)
         response.status(200).json({
           message: comment.message,
-          status: "Message bien envoyé"
+          status: "Commentaire soumis"
         });
       });
     } catch (error) {
