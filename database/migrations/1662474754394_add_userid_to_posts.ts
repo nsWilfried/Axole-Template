655525a0dd07675240800669a4193e9198e69d4e
Wilfried N'SOUKPOE <wilfriednsoukpoe1@gmail.com>
2022-09-08 09:17:56 +0000

    Rebuild

--- database/migrations/1662474754394_add_userid_to_posts.ts
+++ database/migrations/1662474754394_add_userid_to_posts.ts
@@ -4,8 +4,6 @@ export default class AddUseridToPosts extends BaseSchema {
   protected tableName = 'posts'
 
   public async up () {
-    this.schema.dropTableIfExists("comments")
-    this.schema.dropTableIfExists(this.tableName)
       this.schema.createTableIfNotExists(this.tableName, (table) => {
         table.increments('id').primary()
         table.string('name').notNullable()
@@ -14,14 +12,6 @@ export default class AddUseridToPosts extends BaseSchema {
         table.integer('user_id').unsigned().references("users.id")
         table.text('description', 'longtext').notNullable()
         table.text('content','longtext').notNullable
-        table.timestamp('created_at', { useTz: true })
-        table.timestamp('updated_at', { useTz: true })
-      })
-      this.schema.createTableIfNotExists("comments", (table) => {
-        table.increments('id').primary()
-        table.text('message', 'longtext').notNullable()
-        table.integer('post_id').unsigned().references("posts.id").onDelete('CASCADE')
-        table.integer('user_id').unsigned().references("users.id")
         table.timestamp('created_at', { useTz: true })
         table.timestamp('updated_at', { useTz: true })
       })
